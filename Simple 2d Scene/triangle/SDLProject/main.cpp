a952ace809dfe8c1730a921bc37b01c0b9ad3e1c
Anuraghav <anuchan.2004@gmail.com>
2025-02-15 00:02:13 -0500

    stuff

--- Simple 2d Scene/triangle/SDLProject/main.cpp
+++ Simple 2d Scene/triangle/SDLProject/main.cpp
@@ -1,128 +1,155 @@
-/**
- * @file main.cpp
- * @author Sebastián Romero Cruz (src402@nyu.edu)
- * @brief A simple program that creates a window with OpenGL context and 
- * renders a colored triangle. This program demonstrates basic usage of SDL to
- * create a window, create an OpenGL context, and render a simple colored 
- * triangle using shaders.
- * @version 0.1
- * @date 2024-05-20
- * 
- * @copyright Copyright NYU Tandon School of Engineering (c) 2024
- * 
- */
 #define GL_SILENCE_DEPRECATION
-#define GL_GLEXT_PROTOTYPES 1
+#define STB_IMAGE_IMPLEMENTATION
+#define LOG(argument) std::cout << argument << '\n'
 
 #ifdef _WINDOWS
 #include <GL/glew.h>
 #endif
 
+#define GL_GLEXT_PROTOTYPES 1
 #include <SDL.h>
 #include <SDL_opengl.h>
-#include "glm/mat4x4.hpp"                // 4x4 Matrix
-#include "glm/gtc/matrix_transform.hpp"  // Matrix transformation methods
-#include "ShaderProgram.h"               // We'll talk about these later in the course
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+#include "stb_image.h"
+#include "cmath"
+#include <ctime>
 
 enum AppStatus { RUNNING, TERMINATED };
 
-// Our window dimensions
-constexpr int WINDOW_WIDTH  = 640,
-              WINDOW_HEIGHT = 480;
+constexpr float WINDOW_SIZE_MULT = 2.0f;
 
-// Background color components
-constexpr float BG_RED     = 0.1922f,
-                BG_BLUE    = 0.549f,
-                BG_GREEN   = 0.9059f,
+constexpr int WINDOW_WIDTH  = 640 * WINDOW_SIZE_MULT,
+              WINDOW_HEIGHT = 480 * WINDOW_SIZE_MULT;
+
+constexpr float BG_RED     = 0.9765625f,
+                BG_GREEN   = 0.97265625f,
+                BG_BLUE    = 0.9609375f,
                 BG_OPACITY = 1.0f;
 
-// Our viewport—or our "camera"'s—position and dimensions
-constexpr int VIEWPORT_X      = 0,
-              VIEWPORT_Y      = 0,
-              VIEWPORT_WIDTH  = WINDOW_WIDTH,
-              VIEWPORT_HEIGHT = WINDOW_HEIGHT;
+constexpr int VIEWPORT_X = 0,
+          VIEWPORT_Y = 0,
+          VIEWPORT_WIDTH  = WINDOW_WIDTH,
+          VIEWPORT_HEIGHT = WINDOW_HEIGHT;
 
-//Framerate/Delta time
-constexpr float DELTA_TIME = 0.166f;
-float g_previous_ticks = 0.0f;
-float theta = 1.0f;
-// Our shader filepaths; these are necessary for a number of things
-// Not least, to actually draw our shapes 
-// We'll have a whole lecture on these later
-constexpr char V_SHADER_PATH[] = "shaders/vertex.glsl",
-               F_SHADER_PATH[] = "shaders/fragment.glsl";
+constexpr char V_SHADER_PATH[] = "shaders/vertex_textured.glsl",
+           F_SHADER_PATH[] = "shaders/fragment_textured.glsl";
 
-// Our object's fill colour
-constexpr float TRIANGLE_RED     = 1.0,
-                TRIANGLE_BLUE    = 0.4,
-                TRIANGLE_GREEN   = 0.4,
-                TRIANGLE_OPACITY = 1.0;
+constexpr GLint NUMBER_OF_TEXTURES = 1;
+constexpr GLint LEVEL_OF_DETAIL    = 0;
+constexpr GLint TEXTURE_BORDER     = 0;
 
-float g_model_x;
-float g_model_y;
-float g_triangle_rotate = 0.0f;
+constexpr float MILLISECONDS_IN_SECOND = 1000.0;
 
-AppStatus g_app_status = RUNNING;
+constexpr char BEAKER_SPRITE_FILEPATH[] = "beaker.png",
+               DROPS_SPRITE_FILEPATH[]  = "drops.png";
+
+constexpr float MINIMUM_COLLISION_DISTANCE = 1.0f;
+constexpr glm::vec3 INIT_SCALE_DROPS  = glm::vec3(1.0f, 1.0f, 0.0f),
+                    INIT_POS_DROPS    = glm::vec3(-3.0f, 2.0f, 0.0f),
+                    INIT_SCALE_BEAKER = glm::vec3(3.0f, 3.0f, 0.0f),
+                    INIT_POS_BEAKER   = glm::vec3(3.0f, -2.0f, 0.0f);
+
 SDL_Window* g_display_window;
 
-ShaderProgram g_shader_program;
+AppStatus g_app_status = RUNNING;
+ShaderProgram g_shader_program = ShaderProgram();
+glm::mat4 g_view_matrix, g_beaker_matrix, g_projection_matrix, g_drops_matrix;
 
-glm::mat4 g_view_matrix,        // Defines the position (location and orientation) of the camera
-          g_model_matrix,       // Defines every translation, rotation, and/or scaling applied to an object; we'll look at these next week
-          g_projection_matrix;  // Defines the characteristics of your camera, such as clip panes, field of view, projection method, etc.
+float g_previous_ticks = 0.0f;
 
-glm::vec3 g_triangle;
+GLuint g_beaker_texture_id;
+GLuint g_drops_texture_id;
+
+constexpr float DROPS_SPEED = 3.0f; // speed of drop
+
+glm::vec3 g_beaker_position   = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 g_beaker_movement = glm::vec3(0.0f, 0.0f, 0.0f);
+
+glm::vec3 g_drops_position = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 g_drops_movement = glm::vec3(0.0f, 0.0f, 0.0f);
+
+glm::vec3 g_drops_scale = glm::vec3(0.0f, 0.0f, 0.0f);  // scale trigger vector
+glm::vec3 g_drops_size  = glm::vec3(1.0f, 1.0f, 0.0f);  // scale accumulator vector
+
+void initialise();
+void process_input();
+void update();
+void render();
+void shutdown();
+
+GLuint load_texture(const char* filepath)
+{
+    // STEP 1: Loading the image file
+    int width, height, number_of_components;
+    unsigned char* image = stbi_load(filepath, &width, &height, &number_of_components, STBI_rgb_alpha);
+
+    if (image == NULL)
+    {
+        LOG("Unable to load image. Make sure the path is correct.");
+        assert(false);
+    }
+
+    // STEP 2: Generating and binding a texture ID to our image
+    GLuint textureID;
+    glGenTextures(NUMBER_OF_TEXTURES, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, LEVEL_OF_DETAIL, GL_RGBA, width, height, TEXTURE_BORDER, GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+    // STEP 3: Setting our texture filter parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    // STEP 4: Releasing our file from memory and returning our texture id
+    stbi_image_free(image);
+
+    return textureID;
+}
 
 void initialise()
 {
     SDL_Init(SDL_INIT_VIDEO);
-    g_display_window = SDL_CreateWindow("Hello, Triangle!",
+    g_display_window = SDL_CreateWindow("User-Input and Collisions Exercise",
                                       SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                                       WINDOW_WIDTH, WINDOW_HEIGHT,
                                       SDL_WINDOW_OPENGL);
-    
-    if (g_display_window == nullptr)
-    {
-        std::cerr << "ERROR: SDL Window could not be created.\n";
-        g_app_status = TERMINATED;
-        
-        SDL_Quit();
-        exit(1);
-    }
-    
-    g_model_x = 0.0f;
-    g_model_y = 0.0f;
-    
-    g_triangle= glm::vec3(g_model_x, g_model_x,0.0f);
-    
+
     SDL_GLContext context = SDL_GL_CreateContext(g_display_window);
     SDL_GL_MakeCurrent(g_display_window, context);
-    
+
+
+    if (g_display_window == nullptr) shutdown();
+
 #ifdef _WINDOWS
     glewInit();
 #endif
-    
-    // Initialise our camera
+
     glViewport(VIEWPORT_X, VIEWPORT_Y, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
-    
-    // Load up our shaders
+
     g_shader_program.load(V_SHADER_PATH, F_SHADER_PATH);
-    
-    // Initialise our view, model, and projection matrices
-    g_view_matrix       = glm::mat4(1.0f);  // Defines the position (location and orientation) of the camera
-    g_model_matrix      = glm::mat4(1.0f);  // Defines every translation, rotations, or scaling applied to an object
-    g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);  // Defines the characteristics of your camera, such as clip planes, field of view, projection method etc.
-    
+
+    g_beaker_matrix = glm::mat4(1.0f);
+    g_drops_matrix = glm::mat4(1.0f);
+    g_drops_matrix = glm::translate(g_drops_matrix, glm::vec3(1.0f, 1.0f, 0.0f));
+    g_drops_position += g_drops_movement;
+
+    g_view_matrix = glm::mat4(1.0f);
+    g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
+
     g_shader_program.set_projection_matrix(g_projection_matrix);
     g_shader_program.set_view_matrix(g_view_matrix);
-    // Notice we haven't set our model matrix yet!
-    
-    g_shader_program.set_colour(TRIANGLE_RED, TRIANGLE_BLUE, TRIANGLE_GREEN, TRIANGLE_OPACITY);
-    
-    // Each object has its own unique ID
+
     glUseProgram(g_shader_program.get_program_id());
-    
+
     glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
+
+    g_beaker_texture_id = load_texture(BEAKER_SPRITE_FILEPATH);
+    g_drops_texture_id = load_texture(DROPS_SPRITE_FILEPATH);
+
+    // enable blending
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
 void process_input()
@@ -130,71 +157,122 @@ void process_input()
     SDL_Event event;
     while (SDL_PollEvent(&event))
     {
-        if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE)
+        switch (event.type)
         {
-            g_app_status = TERMINATED;
+            // End game
+            case SDL_QUIT:
+            case SDL_WINDOWEVENT_CLOSE:
+                g_app_status = TERMINATED;
+                break;
+
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym)
+                {
+                    case SDLK_q: g_app_status = TERMINATED; break;
+                    default: break;
+                }
+
+            default:
+                break;
         }
+    }
+
+
+    const Uint8 *key_state = SDL_GetKeyboardState(NULL);
+
+    if (glm::length(g_drops_movement) > 1.0f)
+    {
+        // In this particular case, I'm not going to normalise because I am simulating
+        // gravity
+        // g_drops_movement = glm::normalize(g_drops_movement);
     }
 }
 
-void update() {
-    
-    float ticks = (float) SDL_GetTicks() / 1000.0f;
+void update()
+{
+    // --- DELTA TIME CALCULATIONS --- //
+    float ticks = (float) SDL_GetTicks() / MILLISECONDS_IN_SECOND;
     float delta_time = ticks - g_previous_ticks;
     g_previous_ticks = ticks;
-    
-    g_model_x += 0.5f * delta_time;
-    g_model_y += 0.5f * delta_time;
-    g_triangle.x = cos(g_model_x) ;
-    g_triangle.y = sin(g_model_y);
-    
-    g_model_matrix = glm::mat4(1.0f);
-    
-//    g_triangle = glm::vec3(glm::cos(g_model_x), glm::sin(g_model_y), 0.0f);
-    
-    g_model_matrix = glm::translate(g_model_matrix, g_triangle);  // MOVES LINEARLY SIDEWAYS IN DELTATIME
-    
-    
-    
+
+    // --- ACCUMULATOR LOGIC --- //
+    g_drops_position += g_drops_movement * DROPS_SPEED * delta_time;
+    g_drops_size += g_drops_scale * DROPS_SPEED * delta_time;
+
+    // --- TRANSLATION --- //
+    g_beaker_matrix = glm::mat4(1.0f);
+    g_beaker_matrix = glm::translate(g_beaker_matrix, INIT_POS_BEAKER);
+
+    g_drops_matrix = glm::mat4(1.0f);
+    g_drops_matrix = glm::translate(g_drops_matrix, INIT_POS_DROPS);
+    g_drops_matrix = glm::translate(g_drops_matrix, g_drops_position);
+
+    // --- SCALING --- //
+    g_beaker_matrix = glm::scale(g_beaker_matrix, INIT_SCALE_BEAKER);
+
+    g_drops_matrix  = glm::scale(g_drops_matrix, INIT_SCALE_DROPS);
+    g_drops_matrix  = glm::scale(g_drops_matrix, g_drops_size);
+
+    // --- COLLISION LOGIC --- //
+
+
+    // --- TERMINATE GAME IF BEAKER MISSED --- //
+    if (g_drops_position.y < -7.0f) g_app_status = TERMINATED;
+}
+
+void draw_object(glm::mat4 &object_model_matrix, GLuint &object_texture_id)
+{
+    g_shader_program.set_model_matrix(object_model_matrix);
+    glBindTexture(GL_TEXTURE_2D, object_texture_id);
+    glDrawArrays(GL_TRIANGLES, 0, 6); // we are now drawing 2 triangles, so we use 6 instead of 3
 }
 
 void render() {
     glClear(GL_COLOR_BUFFER_BIT);
-    
-    g_shader_program.set_model_matrix(g_model_matrix);
-    
-    float vertices[] =
-    {
-         0.5f, -0.5f,
-         0.0f,  0.5f,
-        -0.5f, -0.5f
+
+    // Vertices
+    float vertices[] = {
+        -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,  // triangle 1
+        -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f   // triangle 2
     };
-    
+
+    // Textures
+    float texture_coordinates[] = {
+        0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,     // triangle 1
+        0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f,     // triangle 2
+    };
+
     glVertexAttribPointer(g_shader_program.get_position_attribute(), 2, GL_FLOAT, false, 0, vertices);
     glEnableVertexAttribArray(g_shader_program.get_position_attribute());
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glVertexAttribPointer(g_shader_program.get_tex_coordinate_attribute(), 2, GL_FLOAT, false, 0, texture_coordinates);
+    glEnableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
+
+    // Bind texture
+    draw_object(g_drops_matrix, g_drops_texture_id);
+    draw_object(g_beaker_matrix, g_beaker_texture_id);
+
+    // We disable two attribute arrays now
     glDisableVertexAttribArray(g_shader_program.get_position_attribute());
-    
+    glDisableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
+
     SDL_GL_SwapWindow(g_display_window);
 }
 
 void shutdown() { SDL_Quit(); }
 
-/**
- Start here—we can see the general structure of a game loop without worrying too much about the details yet.
- */
+
 int main(int argc, char* argv[])
 {
-    // Initialise our program—whatever that means
     initialise();
-    
+
     while (g_app_status == RUNNING)
     {
-        process_input();  // If the player did anything—press a button, move the joystick—process it
-        update();         // Using the game's previous state, and whatever new input we have, update the game's state
-        render();         // Once updated, render those changes onto the screen
+        process_input();
+        update();
+        render();
     }
-    
-    shutdown();  // The game is over, so let's perform any shutdown protocols
+
+    shutdown();
     return 0;
 }
