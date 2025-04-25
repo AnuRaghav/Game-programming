529ba459c0943f27a03a497a304d2493a905c058
Anuraghav <anuchan.2004@gmail.com>
2025-04-24 20:44:59 -0400

    final proj still on level 1

---
+++ Final Project/SDLProject/Entity.cpp
@@ -0,0 +1,411 @@
+int animCols = 10;
+int animRows = 8;
+
+#include "Entity.h"
+#include <iostream>
+using namespace std;
+
+Entity::Entity()
+{
+    position = glm::vec3(0);
+    movement = glm::vec3(0);
+    acceleration = glm::vec3(0);
+    velocity = glm::vec3(0);
+    speed = 0;
+    
+    modelMatrix = glm::mat4(1.0f);
+
+    // Set animation grid for cat.png
+    animCols = 10;
+    animRows = 8;
+
+    // Animation indices for cat (player)
+    animRight = new int[10]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    animLeft = new int[10]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+    animIdle = new int[8]{20, 21, 22, 23, 24, 25, 26, 27};
+    animUseItemRight = new int[8]{30, 31, 32, 33, 34, 35, 36, 37};
+    animUseItemLeft = new int[8]{40, 41, 42, 43, 44, 45, 46, 47};
+    animAttackRight = new int[4]{50, 52, 54, 56};
+    animAttackLeft = new int[4]{60, 62, 64, 66};
+    animIdleLeft = new int[8]{70, 71, 72, 73, 74, 75, 76, 77};
+
+    animIndices = animIdle;
+    animFrames = 4;
+}
+
+bool Entity::CheckCollision(Entity *other) {
+    if (isActive == false || other->isActive == false) return false;
+    float xdist = fabs(position.x - other->position.x) - ((width + other->width) / 2.0f);
+    float ydist = fabs(position.y - other->position.y) - ((height + other->height) / 2.0f);
+    
+    if (xdist < 0 && ydist < 0) return true;
+    return false;
+}
+
+void Entity::CheckCollisionsY(Entity *objects, int objectCount)
+{
+   for (int i = 0; i < objectCount; i++)
+   {
+       Entity *object = &objects[i];
+       if (CheckCollision(object))
+       {
+           float ydist = fabs(position.y - object->position.y);
+           float penetrationY = fabs(ydist - (height / 2.0f) - (object->height / 2.0f));
+           if (velocity.y > 0) {
+               position.y -= penetrationY;
+               velocity.y = 0;
+               collidedTop = true;
+               object->collidedBottom = true;
+           }
+           else if (velocity.y < 0) {
+               position.y += penetrationY;
+               velocity.y = 0;
+               collidedBottom = true;
+               object->collidedTop = true;
+} }
+} }
+
+void Entity::CheckCollisionsX(Entity *objects, int objectCount)
+{
+   for (int i = 0; i < objectCount; i++)
+   {
+       Entity *object = &objects[i];
+       if (CheckCollision(object))
+       {
+           float xdist = fabs(position.x - object->position.x);
+           float penetrationX = fabs(xdist - (width / 2.0f) - (object->width / 2.0f));
+           if (velocity.x > 0) {
+               position.x -= penetrationX;
+               velocity.x = 0;
+               collidedRight = true;
+               object->collidedLeft = true;
+           }
+           else if (velocity.x < 0) {
+               position.x += penetrationX;
+               velocity.x = 0;
+               collidedLeft = true;
+               object->collidedRight = true;
+} }
+} }
+
+void Entity::CheckEnemyCollided(Entity *enemies, int enemyCount) {
+    collidedLeft = false;
+    collidedRight = false;
+    collidedTop = false;
+    CheckCollisionsX(enemies, enemyCount);
+    if (collidedLeft == true || collidedRight == true) isActive = false;
+    CheckCollisionsY(enemies, enemyCount);
+    if (collidedTop == true) isActive = false;
+    if (collidedBottom == true) {
+        for (int i = 0; i < enemyCount; ++i) {
+            if (enemies[i].collidedTop == true) {
+                enemies[i].isActive = false;
+            }
+        }
+    }
+    
+    /*for (int i = 0; i < enemyCount; ++i) {
+        if (CheckCollision(&enemies[i])) {
+            if (collidedBottom && enemies[i].collidedTop) {
+                cout << "enemy dead" << endl;
+                enemies[i].isActive = false;
+            }
+            else {
+                cout << "we ded" << endl;
+                isActive = false;
+                break;
+            }
+        }
+    }*/
+}
+
+void Entity::CheckCollisionsY(Map *map)
+{
+    // Probes for tiles
+    glm::vec3 top = glm::vec3(position.x, position.y + (height / 2), position.z);
+    glm::vec3 top_left = glm::vec3(position.x - (width / 2), position.y + (height / 2), position.z);
+    glm::vec3 top_right = glm::vec3(position.x + (width / 2), position.y + (height / 2), position.z);
+    glm::vec3 bottom = glm::vec3(position.x, position.y - (height / 2), position.z);
+    glm::vec3 bottom_left = glm::vec3(position.x - (width / 2), position.y - (height / 2), position.z);
+    glm::vec3 bottom_right = glm::vec3(position.x + (width / 2), position.y - (height / 2), position.z);
+    float penetration_x = 0;
+    float penetration_y = 0;
+    if (map->IsSolid(top, &penetration_x, &penetration_y) && velocity.y > 0) {
+        position.y -= penetration_y;
+        velocity.y = 0;
+        collidedTop = true;
+    }
+    else if (map->IsSolid(top_left, &penetration_x, &penetration_y) && velocity.y > 0) {
+        position.y -= penetration_y;
+        velocity.y = 0;
+        collidedTop = true;
+    }
+    else if (map->IsSolid(top_right, &penetration_x, &penetration_y) && velocity.y > 0) {
+        position.y -= penetration_y;
+        velocity.y = 0;
+        collidedTop = true;
+    }
+    if (map->IsSolid(bottom, &penetration_x, &penetration_y) && velocity.y < 0) {
+        position.y += penetration_y;
+        velocity.y = 0;
+        collidedBottom = true;
+        if (entityType == PLAYER && map->IsDeathBlock(bottom)) {
+            isActive = false;
+            return;
+        }
+    }
+    else if (map->IsSolid(bottom_left, &penetration_x, &penetration_y) && velocity.y < 0) {
+        position.y += penetration_y;
+        velocity.y = 0;
+        collidedBottom = true;
+        if (entityType == PLAYER && map->IsDeathBlock(bottom_left)) {
+            isActive = false;
+            return;
+        }
+    }
+    else if (map->IsSolid(bottom_right, &penetration_x, &penetration_y) && velocity.y < 0) {
+        position.y += penetration_y;
+        velocity.y = 0;
+        collidedBottom = true;
+        if (entityType == PLAYER && map->IsDeathBlock(bottom_right)) {
+            isActive = false;
+            return;
+        }
+    }
+}
+
+void Entity::CheckCollisionsX(Map *map)
+{
+
+    glm::vec3 left = glm::vec3(position.x - (width / 2), position.y, position.z);
+    glm::vec3 right = glm::vec3(position.x + (width / 2), position.y, position.z);
+    float penetration_x = 0;
+    float penetration_y = 0;
+    if (map->IsSolid(left, &penetration_x, &penetration_y) && velocity.x < 0) {
+        position.x += penetration_x;
+        velocity.x = 0;
+        collidedLeft = true;
+}
+    if (map->IsSolid(right, &penetration_x, &penetration_y) && velocity.x > 0) {
+        position.x -= penetration_x;
+        velocity.x = 0;
+        collidedRight = true;
+} }
+
+void Entity::CheckPit(Entity *platforms, int platformCount) {
+    int leftLocation = -1;
+    int rightLocation = -1;
+    glm::vec3 sensorRight = glm::vec3(position.x + 0.6f, position.y - 0.6f, 0);
+    glm::vec3 sensorLeft = glm::vec3(position.x - 0.6f, position.y - 0.6f, 0);
+    for (int i = 0; i < platformCount; ++i) {
+        float leftDistX = fabs(sensorLeft.x - platforms[i].position.x) - ((width + platforms[i].width) / 2.0f);
+        float rightDistX = fabs(sensorRight.x - platforms[i].position.x) - ((width + platforms[i].width) / 2.0f);
+        float leftDistY = fabs(sensorLeft.y - platforms[i].position.y) - ((height + platforms[i].height) / 2.0f);
+        float rightDistY = fabs(sensorRight.y - platforms[i].position.y) - ((height + platforms[i].height) / 2.0f);
+        
+        if (leftDistX < 0 && leftDistY < 0) leftLocation = i;
+        if (rightDistX < 0 && rightDistY < 0) rightLocation = i;
+    }
+    if (leftLocation == -1) pitLeft = true;
+    if (rightLocation == -1) pitRight = true;
+}
+
+void Entity::AI(Entity *player) {
+    switch(aiType) {
+        case WALKER:
+            break;
+        case WAITANDGO:
+            AIWaitAndGo(player);
+            break;
+        case JUMPER:
+            AIJumper();
+            break;
+    }
+}
+
+void Entity::AIJumper() {
+    if (jump) {
+        jump = false;
+        velocity.y += jumpPower;
+    }
+}
+
+void Entity::AIWalker(Entity *platforms, int platformCount) {
+    if (movement == glm::vec3(0)) {
+        movement = glm::vec3(-1, 0, 0);
+    }
+    CheckPit(platforms, platformCount);
+    if (pitLeft) {
+        movement = glm::vec3(1, 0, 0);
+        pitLeft = false;
+    }
+    if (pitRight) {
+        movement = glm::vec3(-1, 0, 0);
+        pitRight = false;
+    }
+}
+
+void Entity::AIWaitAndGo(Entity *player) {
+    switch(aiState) {
+        case IDLE:
+            if (glm::distance(position, player->position) < 3.0f) {
+                aiState = WALKING;
+            }
+            break;
+            
+        case WALKING:
+            if (player->position.x < position.x) {
+                movement = glm::vec3(-1, 0, 0);
+            }
+            else {
+                movement = glm::vec3(1, 0, 0);
+            }
+            break;
+            
+        case JUMPING:
+            break;
+            
+        case ATTACKING:
+            break;
+    }
+}
+
+void Entity::Update(float deltaTime, Entity *player, Entity *objects, int objectCount, Map *map)
+{
+    if (isActive == false) {
+        return;
+    }
+    collidedTop = false;
+    collidedBottom = false;
+    collidedLeft = false;
+    collidedRight = false;
+    
+    pitLeft = false;
+    pitRight = false;
+    
+    if (entityType == ENEMY) {
+        AI(player);
+    } else if (entityType == BAT) {
+        batAngle += batSpeed * deltaTime;
+        position.x = batOrigin.x + cos(batAngle) * batRadius;
+        position.y = batOrigin.y + sin(batAngle) * batRadius;
+    }
+    
+    if (animIndices != NULL) {
+        animTime += deltaTime;
+        if (animTime >= 0.1f) {
+            animTime = 0.0f;
+            animIndex++;
+            if (animIndex >= animFrames) {
+                if (isAttacking) {
+                    isAttacking = false;
+                }
+                animIndex = 0;
+            }
+        }
+    }
+    
+    if (!isAttacking && movement.x == 0 && movement.y == 0) {
+        if (facingLeft && animIndices != animIdleLeft) {
+            animIndices = animIdleLeft;
+            animFrames = 8;
+            animIndex = 0;
+            animTime = 0.0f;
+        } else if (!facingLeft && animIndices != animIdle) {
+            animIndices = animIdle;
+            animFrames = 8;
+            animIndex = 0;
+            animTime = 0.0f;
+        }
+    }
+    
+    if (jump) {
+        jump = false;
+        velocity.y += jumpPower;
+    }
+    if (entityType == PLAYER && map->IsJumpPad(position)) {
+        position.x += 6.0f;
+        velocity.x = 10.0f; // Launch with high speed to the right
+    }
+    velocity.x = movement.x * speed;
+    velocity.y = movement.y * speed;
+    velocity += acceleration * deltaTime;
+    
+    position.y += velocity.y * deltaTime;
+    CheckCollisionsY(map);
+    
+    position.x += velocity.x * deltaTime;
+    CheckCollisionsX(map);
+    
+    if (entityType == PLAYER && map->IsDeathBlock(position)) {
+        isActive = false;
+        return;
+    }
+    
+    if(entityType == PLAYER) {
+        //CheckCollisionsY(objects, objectCount);
+        //CheckCollisionsX(objects, objectCount);
+        CheckEnemyCollided(objects, objectCount);
+    } else if (entityType == BAT && player != nullptr && CheckCollision(player)) {
+        player->isActive = false;
+    }
+    
+    modelMatrix = glm::mat4(1.0f);
+    modelMatrix = glm::translate(modelMatrix, position);
+}
+
+void Entity::DrawSpriteFromTextureAtlas(ShaderProgram *program, GLuint textureID, int index)
+{
+    float u = (float)(index % animCols) / (float)animCols;
+    float v = (float)(index / animCols) / (float)animRows;
+    
+    float width = 1.0f / (float)animCols;
+    float height = 1.0f / (float)animRows;
+    
+    float texCoords[] = { u, v + height, u + width, v + height, u + width, v,
+        u, v + height, u + width, v, u, v};
+    
+    float vertices[]  = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+    
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    
+    glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program->positionAttribute);
+    
+    glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+    glEnableVertexAttribArray(program->texCoordAttribute);
+    
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    
+    glDisableVertexAttribArray(program->positionAttribute);
+    glDisableVertexAttribArray(program->texCoordAttribute);
+}
+
+void Entity::Render(ShaderProgram *program) {
+    if (isActive == false) {
+        return;
+    }
+    
+    program->SetModelMatrix(modelMatrix);
+    
+    if (animIndices != NULL) {
+        DrawSpriteFromTextureAtlas(program, textureID, animIndices[animIndex]);
+        return;
+    }
+    
+    float vertices[]  = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+    float texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+    
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    
+    glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program->positionAttribute);
+    
+    glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+    glEnableVertexAttribArray(program->texCoordAttribute);
+    
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    
+    glDisableVertexAttribArray(program->positionAttribute);
+    glDisableVertexAttribArray(program->texCoordAttribute);
+}
---
+++ Final Project/SDLProject/Level3.cpp
@@ -0,0 +1,168 @@
+#include "Level3.h"
+#define Level3_WIDTH 30
+#define Level3_HEIGHT 20
+
+#define LEVEL3_ENEMY_COUNT 3
+using namespace std;
+
+int level3_data[] = {
+   -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+   -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+   -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+   -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+   -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+   -1, -1, -1, -1, -1, -1, -1, -1, -1, 15122, 15123, 15123, 15123, 15123, 15123, 15123, 15123, 15123, 15123, 15123, 15124, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+   -1, -1, -1, -1, -1, -1, 15122, 15123, 15123, 15661, 16216, 16217, 16218, 16217, 16217, 16217, 16217, 16217, 16217, 16217, 15660, 15123, 15123, 15124, -1, -1, -1, -1, -1, -1,
+   -1, -1, -1, -1, -1, -1, 15392, 16217, 16217, 16217, 16487, 16487, 16487, 16487, 16487, 16487, 16487, 16487, 16487, 16487, 16217, 16217, 16217, 15394, -1, -1, -1, -1, -1, -1,
+   -1, -1, -1, -1, -1, -1, 15392, 16487, 16487, 16487, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 16487, 16487, 16487, 15394, -1, -1, -1, -1, -1, -1,
+   -1, -1, -1, -1, -1, -1, 15392, 18917, 18917, 18917, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18917, 18917, 18917, 15394, -1, -1, -1, -1, -1, -1,
+   -1, -1, -1, -1, -1, -1, 15392, 18917, 18917, 18917, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18917, 18917, 18917, 15394, -1, -1, -1, -1, -1, -1,
+   -1, -1, -1, -1, -1, 15143, 15392, 18917, 18917, 18917, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18917, 18917, 18917, 15394, -1, -1, -1, -1, -1, -1,
+   -1, -1, -1, -1, -1, -1, 15392, 18917, 18917, 18917, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18917, 18917, 18917, 15394, -1, -1, -1, -1, -1, -1,
+   -1, -1, -1, -1, -1, -1, 15662, 15663, 15663, 15391, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 18914, 15390, 15663, 15663, 15664, -1, -1, -1, -1, -1, -1,
+   -1, -1, -1, -1, -1, -1, -1, -1, -1, 15662, 15663, 15663, 15663, 15663, 15663, 15663, 15663, 15663, 15663, 15663, 15664, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+   -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+   -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+   -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+   -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+   -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
+
+};
+
+int level3_furnitureData[] = {
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 20336, -1, 20600, 20357, 20358, 27642, 27643, 17356, 33509, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, 1698, 1699, 1700, 20597, 20062, -1, 37269, 37270, -1, 27912, 27913, 17626, 33509, 1158, 1159, 1160, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, 1968, 1969, 1970, -1, -1, -1, 37539, 37540, -1, 31670, 31671, -1, 33779, 1428, 1429, 1430, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, 19002, -1, -1, 37577, 30860, 21610, 21611, 21611, 21612, 37574, 37575, -1, -1, -1, -1, 20063, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, 19272, -1, -1, -1, -1, 21880, 21881, 21881, 21882, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, 25197, -1, -1, -1, -1, 55622, 55623, 55624, 55625, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, 25467, -1, -1, 25730, 25731, 55892, 55893, 55894, 55895, -1, 17046, -1, 25737, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 26000, 26001, 26270, 26271, -1, -1, -1, -1, -1, 26007, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
+};
+void Level3::Initialize(int numLives) {
+    
+    state.nextScene = -1;
+    
+    GLuint mapTextureID = Util::LoadTexture("tileset.png");
+    state.map = new Map(Level3_WIDTH,Level3_HEIGHT, level3_data, level3_furnitureData, mapTextureID, 1.0f, 270, 215);
+    // Initialize Player
+    state.player = new Entity();
+    state.player->entityType = PLAYER;
+    state.player->position = glm::vec3(1, -2, 0);
+    state.player->movement = glm::vec3(0);
+    state.player->acceleration = glm::vec3(0, -9.81f, 0);
+    state.player->speed = 3.0f;
+    state.player->textureID = Util::LoadTexture("micheal.png");
+    
+    state.player->animRight = new int[4] {3, 7, 11, 15};
+    state.player->animLeft = new int[4] {1, 5, 9, 13};
+    state.player->animUp = new int[4] {2, 6, 10, 14};
+    state.player->animDown = new int[4] {0, 4, 8, 12};
+    
+    state.player->animIndices = state.player->animRight;
+    state.player->animFrames = 4;
+    state.player->animIndex = 0;
+    state.player->animTime = 0;
+    state.player->animCols = 4;
+    state.player->animRows = 4;
+    
+    state.player->numLives = numLives;
+    
+    state.player->height = 0.8f;
+    state.player->width = 0.8f;
+    
+    state.player->jumpPower = 7.0f;
+    
+    state.enemies = new Entity[LEVEL3_ENEMY_COUNT];
+    GLuint enemyTextureID = Util::LoadTexture("bug.png");
+    
+    state.enemies[0].entityType = ENEMY;
+    state.enemies[0].textureID = enemyTextureID;
+    state.enemies[0].position = glm::vec3(24, -2.25, 0);
+    state.enemies[0].speed = 1;
+    state.enemies[0].aiType = WAITANDGO;
+    state.enemies[0].aiState = IDLE;
+    state.enemies[0].jumpPower = 5.0f;
+    state.enemies[0].acceleration = glm::vec3(0, -9.81, 0);
+    
+    GLuint batTextureID = Util::LoadTexture("bat.png");
+    if (batTextureID == 0) {
+        batTextureID = Util::LoadTexture("fallback.png"); // optional fallback texture
+    }
+
+    state.enemies[1].entityType = BAT;
+    state.enemies[1].textureID = batTextureID;
+    state.enemies[1].batOrigin = glm::vec3(15, -3, 0);
+    state.enemies[1].position = state.enemies[1].batOrigin;
+    state.enemies[1].isActive = true;
+    state.enemies[1].animRight = new int[4]{0, 1, 2, 3};
+    state.enemies[1].animIndices = state.enemies[1].animRight;
+    state.enemies[1].animFrames = 4;
+    state.enemies[1].animIndex = 0;
+    state.enemies[1].animTime = 0;
+    state.enemies[1].animCols = 4;
+    state.enemies[1].animRows = 4;
+
+    state.enemies[2].entityType = BAT;
+    state.enemies[2].textureID = batTextureID;
+    state.enemies[2].batOrigin = glm::vec3(28, -3.5, 0);
+    state.enemies[2].position = state.enemies[2].batOrigin;
+    state.enemies[2].isActive = true;
+    state.enemies[2].animRight = new int[4]{0, 1, 2, 3};
+    state.enemies[2].animIndices = state.enemies[2].animRight;
+    state.enemies[2].animFrames = 4;
+    state.enemies[2].animIndex = 0;
+    state.enemies[2].animTime = 0;
+    state.enemies[2].animCols = 4;
+    state.enemies[2].animRows = 4;
+
+    for (int i = 0; i < LEVEL3_ENEMY_COUNT; ++i) {
+        state.enemies[i].isActive = true;
+    }
+}
+void Level3::Update(float deltaTime) {
+    state.player->Update(deltaTime, state.player, state.enemies, LEVEL3_ENEMY_COUNT, state.map);
+    for (int i = 0; i < LEVEL3_ENEMY_COUNT; ++i) {
+        state.enemies[i].Update(deltaTime, state.player, state.enemies, LEVEL3_ENEMY_COUNT, state.map);
+    }
+    if (state.player->numLives == 0) {
+        state.nextScene = 4;
+    }
+    bool anyAlive = false;
+    for (int i = 0; i < LEVEL3_ENEMY_COUNT; ++i) {
+        if (state.enemies[i].isActive == true) {
+            anyAlive = true;
+            break;
+        }
+    }
+    if (state.player->position.x >= 33 && state.player->position.y <= -3) {
+        state.nextScene = 5;
+    }
+    if (state.player->position.y <= -10 || state.player->isActive == false) {
+        state.player->isActive = true;
+        state.player->numLives -= 1;
+        state.player->position = glm::vec3(1, -2, 0);
+    }
+    
+}
+void Level3::Render(ShaderProgram *program) {
+    Util::DrawText(program, Util::LoadTexture("font1.png"), "Level 3", 0.5f, -0.1f, glm::vec3(0, -2, 0));
+    Util::DrawText(program, Util::LoadTexture("font1.png"), "Lives " + std::to_string(state.player->numLives), 0.5f, -0.1f, glm::vec3(0, -2.5, 0));
+    state.map->Render(program);
+    state.player->Render(program);
+    for (int i = 0; i < LEVEL3_ENEMY_COUNT; ++i) {
+        state.enemies[i].Render(program);
+    }
+}
